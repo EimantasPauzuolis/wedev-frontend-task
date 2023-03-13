cdf3edb49c76887c8edb6cb60f11597889c8bdc0
EimantasPauzuolis <eimantas.pauzuolis@gmail.com>
2023-03-13 11:24:35 +0200

    ÄCreate project.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class='text-4xl text-center'>
+    Header
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup lang='ts'>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
